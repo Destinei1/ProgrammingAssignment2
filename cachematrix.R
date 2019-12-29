fe25b8afd577ba1f2671f67f77e44ada46d1c25b
Destinei1 <Destineisimp@gmail.com>
2019-12-29 12:51:23 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,26 +1,34 @@
 ## Matrix Get and Caching
 ## 
 
-## First I is set to Null 
-## Then the next inside function set the current matrix, and sets "I"
+## Start with an empty matrix
+## Initialize M and set to Null
 ## to Null if there was a previous Inverse stored
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## Initialize M and set to Null
   I <- NULL
+  ## Assign input argument to X object in parent environment and set I to Null in case of 
+  ## previous Cached inverse
   set <- function(y) {
     x <<- y
     I <<- NULL
   }
+  ## retrives the value of x from parent environment
   get <- function() x
+  ## assigns the input argument to I
   setInv <- function(Inv) I <<- Inv
+  ## retriveves the value of I from the parent evironment
   getInv <- function() I
+  ## Creates list so we can use $ operator to extract
   list(set = set, get = get,
        setInv = setInv,
        getInv = getInv)}
 
 
-## Write a short comment describing this function
+## Uses the previous function output functions to set the Inverse to I
+## if I isn't NULL then it pulls from Cache
+## If I is NULL then it computes the inverse and sets new inverse to I
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
@@ -30,9 +38,6 @@ cacheSolve <- function(x, ...) {
     return(I)
   }
   data <- x$get()
-  {
-    message("Can't compute, Determite is equal to 0")
-  }
   I <- solve(data, ...)
   x$setInv(I)
   I
